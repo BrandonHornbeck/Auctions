c0b2d29642a0b4e2e7a5f3f690b46c0f1c1dd9d3
zyno1 <olivierzeyen@gmail.com>
2021-02-26 14:52:19 +0100

    order start

---
+++ src/main/java/fr/univlorraine/auctions/beans/managers/DeliveryBean.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fr.univlorraine.auctions.beans.managers;
+
+import javax.annotation.Resource;
+import javax.ejb.ActivationConfigProperty;
+import javax.ejb.MessageDriven;
+import javax.ejb.MessageDrivenContext;
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.MessageListener;
+
+/**
+ *
+ * @author oz
+ */
+@MessageDriven(activationConfig = {
+    @ActivationConfigProperty(
+        propertyName = "acknowledgeMode",
+        propertyValue = "Auto-acknowledge"),
+    @ActivationConfigProperty(
+        propertyName = "destinationType",
+        propertyValue = "javax.jms.Queue"),
+    @ActivationConfigProperty(
+        propertyName = "destinationLookup",
+        propertyValue = "jms/OrderQueue"),
+})
+public class DeliveryBean implements MessageListener {
+    
+    @Resource
+    private MessageDrivenContext context;
+    
+    @Override
+    public void onMessage(Message msg) {
+        try {
+            Order o = msg.getBody(Order.class);
+            System.out.println("Delivering: " + o.toString());
+        } catch (JMSException ex) {
+            ex.printStackTrace();
+            context.setRollbackOnly();
+        }
+    }
+    
+}
